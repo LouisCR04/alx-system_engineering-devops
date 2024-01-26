504e7941fe26ab7312bcd994e2129c0f5069a139
LouisCR04 <louiskariuki04@gmail.com>
2024-01-26 20:17:06 +0300

    Puppet-lint compliance

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Connects to a server using SSH without Password
 
-exec { 'echo "PasswordAuthentification no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
-	path	=> '/bin/'
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+        path    => '/bin/'
 }
