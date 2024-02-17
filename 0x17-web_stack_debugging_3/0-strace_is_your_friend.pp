592b662fb67aa9f5d846b1c6c4e5c641a530ba44
LouisCR04 <louiskariuki04@gmail.com>
2024-02-17 18:42:11 +0300

    Initial commit

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# What server, wth is going on here, score too low to wait to catch up
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
