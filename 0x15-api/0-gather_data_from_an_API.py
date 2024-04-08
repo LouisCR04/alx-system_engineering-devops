549b48f0d8614ed6162010cefbbb3525ef6e7b83
LouisCR04 <louiskariuki04@gmail.com>
2024-04-08 15:56:42 +0300

    Initial Commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""0-gather_data_from_an_API.py"""
+
+import requests
+import sys
+
+url = "https://jsonplaceholder.typicode.com/"
+user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+completed = [t.get("title") for t in todos if t.get("completed") is True]
+
+print("Employee {} is done with tasks {}/{}):".format(
+    user.get("name"), len(completed), len(todos)))
+
+for tasks in completed:
+    print("\t {}".format(tasks))
