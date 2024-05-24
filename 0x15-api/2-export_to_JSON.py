3e3d6793f2ad34d7ec3b15ae184e6262064675d2
LouisCR04 <louiskariuki04@gmail.com>
2024-05-24 11:17:25 +0300

    Export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# 2-export_to_JSON.py
+
+import json
+import requests
+import sys
+
+
+def gather_data():
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    user = requests.get(url + f"users/{user_id}").json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    tasks = []
+    for todo in todos:
+        tasks.append({
+            "task": todo['title'],
+            "completed": todo['completed'],
+            "username": user['username']
+        })
+
+    with open(user_id + '.json', mode='w') as file:
+        json.dump({user['id']: tasks}, file)
+
+
+if __name__ == "__main__":
+    gather_data()
